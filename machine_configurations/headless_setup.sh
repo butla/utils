effec637d43130514845f225fdbe7eef3078c94e
Michal Bultrowicz <michalbultrowicz@gmail.com>
2017-04-14 17:34:51 +0200

    Ansible: fish and reorganization

---
+++ machine_configurations/headless_setup.sh
@@ -0,0 +1,1 @@
+ANSIBLE_NOCOWS=1 ansible-playbook laptop.yml -i localhost --ask-become-pass
---
+++ machine_configurations/roles/small_dev/files/fish/config.fish
@@ -0,0 +1,6 @@
+set fish_greeting ""
+set PATH $HOME/.local/bin $PATH
+
+eval (python3 -m virtualfish)
+# for virtualenv activation using only my prompt
+set VIRTUAL_ENV_DISABLE_PROMPT yes
